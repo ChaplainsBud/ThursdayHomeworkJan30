2cf6178ffc8077aab44b059167d11e2f0baaf4b3
GBTC441010UR <GBTC441010UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-01-30 12:59:27 -0500

    Stuck on #6

---
+++ src/com/company/Main.java
@@ -0,0 +1,64 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //        //#1
+//        //i. get num
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter number: ");
+//        int num = sc.nextInt();
+//        //ii. loop num from 1 to num
+//        for (int i = 1; i <= num; i++){
+//            System.out.println(i * i * i);
+//        }
+//
+        // #2
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter number: ");
+//        int num = sc.nextInt();
+//        //ii. loop num from 1 to num
+//        for (int i = 1; i <= 10; i++){
+//            System.out.println(i + " x " + num + " = " + i * num);
+//        }
+//        //#3
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter number: ");
+//        int num = sc.nextInt();
+//        int total = 0;
+//        for(int i = 1; i <= num; i+=2 ){
+//            total +=i;
+//            System.out.println(i);
+//        }
+//        System.out.println("The sum of these numbers equals " + total);
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter number: ");
+//        int num = sc.nextInt();
+//        int total = 0;
+//        for(int i = 2; i <= num; i+=2 ){
+//            total +=i;
+//            System.out.println(i);
+//        }
+//        System.out.println("The sum of these numbers equals " + total);
+
+     //    #5 There is no 5, skips to 6
+// #6 NOT FINISHED
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter number of rows: ");
+//        int num = sc.nextInt();
+// #7 NOT FINISHED
+
+// #8 NOT FINISHED
+
+// #9 NOT FINISHED
+
+// #10 NOT FINISHED
+
+// #11 NOT FINISHED
+
+    }
+}
