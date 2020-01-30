5a96cf81f1d92f31f948f8b0ee8ba4c0ee3baeea
GBTC441010UR <GBTC441010UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-01-30 15:39:22 -0500

    finished six and seven.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,21 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        //        //#1
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number: ");
+        int num = sc.nextInt();
+        for (int i = 1; i <= num; i++){
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println("");
+        }
+
+
+
+        // write your code here
+            //        //#1
 //        //i. get num
 //        Scanner sc = new Scanner(System.in);
 //        System.out.println("Enter number: ");
@@ -16,7 +29,7 @@ public class Main {
 //            System.out.println(i * i * i);
 //        }
 //
-        // #2
+            // #2
 //        Scanner sc = new Scanner(System.in);
 //        System.out.println("Enter number: ");
 //        int num = sc.nextInt();
@@ -45,14 +58,51 @@ public class Main {
 //        }
 //        System.out.println("The sum of these numbers equals " + total);
 
-     //    #5 There is no 5, skips to 6
-// #6 NOT FINISHED
-//        Scanner sc = new Scanner(System.in);
-//        System.out.println("Enter number of rows: ");
-//        int num = sc.nextInt();
-// #7 NOT FINISHED
+            //    #5 There is no 5, skips to 6
+// #6
 
-// #8 NOT FINISHED
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter number: ");
+//        int num = sc.nextInt();
+//
+
+        // the outer loop is bound to the num
+        // the inner loop is bound to the outer loop
+        // i goes to num; j goes to i
+        // i can go to any chosen num; j is always contingent on j
+
+//        for (int i = 1; i <= num; i++){
+//            for(int j = 1; j <= i; j++){
+//                // j + space
+//                // every time counting from 1 to i; as i changes, j counts more
+//                System.out.print(j);
+//            }   // next line
+//            System.out.println("");
+//        }
+
+
+// #7
+
+
+        // outer loop, change lines
+        // inner loop, not counting from 1 - i, but repeating i ... i amount of times
+
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter number: ");
+//        int num = sc.nextInt();
+//        for(int i = 1; i <= num; i++){
+//            for(int j = 1; j <= i; j++) {
+//                System.out.print(i);
+//            }
+//                System.out.println("");
+//        }
+
+// #8
+
+// inner loop: print numbers to upward bound number, with incremented amount -->
+
+
 
 // #9 NOT FINISHED
 
@@ -60,5 +110,5 @@ public class Main {
 
 // #11 NOT FINISHED
 
-    }
+   }
 }
